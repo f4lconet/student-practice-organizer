c8e11274b2fa5b0117b024dd459b84058a958814
Alexander Sokolov <sokol2006312@mail.ru>
2026-07-20 20:22:52 +0500

    feat: Добавление файлов для деплоя и изменение путей фронтенда на относительные

---
+++ Dockerfile
@@ -0,0 +1,20 @@
+FROM node:22-alpine
+
+WORKDIR /app
+
+COPY . .
+
+RUN npm install
+
+RUN npm run build
+
+RUN cd packages/backend && npx prisma generate && npx prisma migrate deploy
+
+RUN apk add --no-cache nginx supervisor
+
+COPY nginx.conf /etc/nginx/http.d/default.conf
+COPY supervisord.conf /etc/supervisord.conf
+
+EXPOSE 10000
+
+CMD ["/usr/bin/supervisord","-c","/etc/supervisord.conf"]
--- packages/frontend/next.config.ts
+++ packages/frontend/next.config.ts
@@ -1,18 +1,5 @@
 import type { NextConfig } from "next";
 
-const nextConfig: NextConfig = {
-  async rewrites() {
-    return [
-      {
-        source: "/api/:path*",
-        destination: `${process.env.BACKEND_URL}/:path*`,
-      },
-      {
-        source: "/uploads/:path*",
-        destination: `${process.env.BACKEND_URL}/uploads/:path*`,
-      },
-    ];
-  },
-};
+const nextConfig: NextConfig = {};
 
 export default nextConfig;
